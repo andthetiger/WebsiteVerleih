2289586615bf8982c0547a6d3c22ca25b0e3c426
AndreasF <andreas.fuerer@zhdk.ch>
2019-06-09 22:48:28 +0200

    working prototype with interaction
    to be submitted

--- scripts/script.js
+++ scripts/script.js
@@ -57,4 +57,22 @@ $(document).ready(function() {
 
   });
 
+  $('.my-custom-button2').click(function(event) {
+    $button = $(event.target);
+
+    // change the buttons text
+    $button.text('added');
+
+    // get data attribute to revel correct item in cart
+    var itemName = $button.attr('data-item');
+    console.log('itemName: ', itemName);
+
+    // get the right item to reveal
+    var $itemToReveal = $('#' + itemName);
+    console.log('$itemToReveal: ', $itemToReveal);
+
+    $itemToReveal.removeClass('is-hidden');
+
+  });
+
 });
